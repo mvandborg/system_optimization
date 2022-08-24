97f4f31b976d6e028c4be67e1458dc4a4a0f3264
WIN\madshv <mads-vandborg@hotmail.com>
2022-08-24 15:45:53 +0200

    Physical parameters moved to separate file.

--- distsensor_optimization.py
+++ distsensor_optimization.py
@@ -29,72 +29,13 @@ from src.fiberdata_passive import Passivefiber_class
 from src.simulation_system import System_simulation_class
 c = c*1e-9              # Unit m/ns
 
-# %% Define physical parameters
-Pp0 = 1.35              # Pump power (W)
-Ppr0 = 0.062           # Probe power (W)
-lam_p = 1480e-9         # Wavelength (m)
-lam_pr = 1550e-9
-
-f_p = c/lam_p           # Frequencies (GHz)
-f_pr = c/lam_pr
-
-f_delta = f_p-f_pr      
-
-# Brillouin parameters
-Tpulse = 50             # Pulse duration (ns)
-T = 300                 # Temperature (K)
-f_b = 10.8              # Brillouin frequency shift (GHz)
-FWHM_b = 38e-3          # FWHM of Brillouin peak (GHz)
-Gamma_b = 2*pi*FWHM_b
-ng = 1.45
-vg = c/ng               # Group velocity (m/ns)
-g_b = 0.1470            # Brillouin gain factor(1/W/m)
-
-# %% Define fibers
-gamma_raman = 1.72e-14  # Raman gain coefficient (m/W)
-df_raman = f_p-f_pr
-
-# Sumitomo Z Fiber LL
-alpha_db_p1 = 0.195         # Fiber loss (dB/km)
-alpha_db_pr1 = 0.161
-D_p1 = 13                   # GVD param (ps/(nm*km))
-D_pr1 = 17
-Aeff1 = 85e-12
-Fiber_SumLL = Passivefiber_class(np.array([lam_p,lam_pr]),\
-                           np.array([alpha_db_p1,alpha_db_pr1]),\
-                           np.array([D_p1,D_pr1]),\
-                           np.array([Aeff1,Aeff1]))
-Fiber_SumLL.add_raman(df_raman,gamma_raman/Aeff1)
-    
-# Sumitomo Z-PLUS Fiber ULL
-alpha_db_p1 = 0.180         # Fiber loss (dB/km)
-alpha_db_pr1 = 0.153
-D_p1 = 16                   # GVD param (ps/(nm*km))
-D_pr1 = 20
-Aeff1 = 112e-12
-Fiber_SumULL = Passivefiber_class(np.array([lam_p,lam_pr]),\
-                           np.array([alpha_db_p1,alpha_db_pr1]),\
-                           np.array([D_p1,D_pr1]),\
-                           np.array([Aeff1,Aeff1]))
-Fiber_SumULL.add_raman(df_raman,gamma_raman/Aeff1)
-    
-# Sumitomo Z-PLUS Fiber 150
-alpha_db_p1 = 0.180         # Fiber loss (dB/km)
-alpha_db_pr1 = 0.150
-D_p1 = 17                   # GVD param (ps/(nm*km))
-D_pr1 = 21
-Aeff1 = 150e-12
-Fiber_Sum150 = Passivefiber_class(np.array([lam_p,lam_pr]),\
-                           np.array([alpha_db_p1,alpha_db_pr1]),\
-                           np.array([D_p1,D_pr1]),\
-                           np.array([Aeff1,Aeff1]))
-Fiber_Sum150.add_raman(df_raman,gamma_raman/Aeff1)
-
-dir_edf = r'C:/Users/madshv/OneDrive - Danmarks Tekniske Universitet/fiber_data/ofs_edf/'
-file_edf = r'LP980_11841.s'
-Fiber_edf = Erbiumfiber_class.from_ofs_files(dir_edf, file_edf)
+# Import physical parameters
+from physical_parameters import *
 
 # %% Fiber section parameters
+Pp0 = 1.35              # Pump power (W)
+Ppr0 = 0.062           # Probe power (W)
+
 L0 = 150e3
 L_co = [1]
 L_edf = [9]
